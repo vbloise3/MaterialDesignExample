d917edd1af6bb3171fa04a02e425cc7fb316732c
Vince Bloise <vbloise3@verizon.net>
2017-09-07 21:57:06 -0400

    Added basic table!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdTableModule} from '@angular/material';
+
+import {BasicComponent} from './basic/basic.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, BasicComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule, MdTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
